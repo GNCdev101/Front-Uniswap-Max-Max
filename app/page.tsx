62fec472fb529bad005d9260949460f8f5daed39
ekkaia <ekkaiasmith@gmail.com>
2023-05-30 02:42:48 +0200

    introduction

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,37 @@ export default function Home() {
 	}, []);
 
 	return (
-		<div className="w-full">
+		<div className="w-screen h-full">
 			<GradientCanvas>
 				<Gradient type={type} color1="#8903ff" color2="#712b31" color3="#350062" uSpeed={0.1} cDistance={4} />
 			</GradientCanvas>
-			<TextReveal />
+			<div className="front-page-content">
+				<div className="flex justify-center items-center h-screen">
+					<TextReveal />
+				</div>
+				<div className="fit-content h-screen pt-32">
+					<div className="flex flex-col justify-center items-center text-center gap-4">
+						<h2 className="text-neutral-300 text-5xl leading-relaxed" style={{fontStretch: "expanded"}}>
+							Introducing Uniswap Max
+						</h2>
+						<p className="text-neutral-400 text-3xl leading-snug">A Decentralized Trading Platform<br /> Built on Top of Uniswap!</p>
+					</div>
+					{/* <div className="front-page-introduction rounded-3xl p-8 flex flex-col gap-4 text-4xl text-neutral-300 leading-loose">
+						<p>
+							Introducing Uniswap Max, a decentralized trading platform built on top of Uniswap!
+						</p>
+						<p>
+							Uniswap Max is the next generation of decentralize trading tools. We've built it to be as easy-to-use as possible, so even if you're new to crypto trading, we'll have your back.
+						</p>
+						<p>
+							Our features include: stop loss, limit orders and margin trading—so you can set your own rules for each trade. We also offer leverage and shorting options, which give you the opportunity to earn bigger profits (or lose more money).
+						</p>
+						<p>
+							Even if you're not a trader, you can still unlock the power of Uniswap Max by becoming a liquidity provider to margin traders and earning a competitive APY.
+						</p>
+					</div> */}
+				</div>
+			</div>
 		</div>
 	);
 }
